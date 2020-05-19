9da62c858321ad3e94be6b0a16773d17535559b2
Franto <frantoshajel@gmail.com>
2020-05-19 15:12:38 +0530

    added response and security

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.mysql.cj.protocol.Resultset;
 import io.swagger.oas.models.ExternalDocumentation;
 import io.swagger.oas.models.OpenAPI;
 import io.swagger.oas.models.Operation;
@@ -9,6 +10,9 @@ import io.swagger.oas.models.media.MediaType;
 import io.swagger.oas.models.media.Schema;
 import io.swagger.oas.models.parameters.Parameter;
 import io.swagger.oas.models.parameters.RequestBody;
+import io.swagger.oas.models.responses.ApiResponse;
+import io.swagger.oas.models.responses.ApiResponses;
+import io.swagger.oas.models.security.SecurityRequirement;
 import io.swagger.oas.models.tags.Tag;
 
 import java.sql.*;
@@ -23,7 +27,11 @@ public class Main {
 
         Connection con = null;
         Statement stmt = null;
+        Statement stmt1 = null;
         Statement stmt2 = null;
+        Statement stmt3 = null;
+//        Statement stmt4 = null;
+//        Statement stmt5 = null;
 
         OpenAPI petstore = new OpenAPI();
 
@@ -35,7 +43,14 @@ public class Main {
             System.out.println("Connected database successfully...");
             System.out.println("Creating statement...");
             stmt = con.createStatement();
+            stmt1 = con.createStatement();
+            stmt2 = con.createStatement();
+            stmt3 = con.createStatement();
+//            stmt4 = con.createStatement();
+//            stmt5 = con.createStatement();
 
+            //info:
+            System.out.println("Info started");
             String query = "SELECT * FROM info";
             ResultSet info_rs = stmt.executeQuery(query);
 
@@ -58,6 +73,11 @@ public class Main {
 
 //            System.out.println(petstore.getInfo());
             info_rs.close();
+            System.out.println("Info Ended");
+
+
+            //Tags:
+            System.out.println("Tags Started");
 
             String tags_query = "SELECT * FROM tags";
             ResultSet tags_rs = stmt.executeQuery(tags_query);
@@ -82,12 +102,15 @@ public class Main {
 //            System.out.println(petstore.getTags());
             tags_rs.close();
 
+            System.out.println("Tags Ended");
+
             //Paths:
+            System.out.println("Paths Started");
             String paths_query = "SELECT * FROM Paths";
             ResultSet paths_rs = stmt.executeQuery(paths_query);
 //            String path = "";
 
-//            while (rs.next()) {
+//            while (paths_rs.next()) {
                 paths_rs.next();
 
                 Operation pet_put = new Operation();
@@ -107,11 +130,12 @@ public class Main {
 
 
                         //tags
+            System.out.println("Path Tags started");
                         String paramTags_query = "SELECT Paths.PathID, TagID, Tag FROM Paths \n" +
                                 "LEFT JOIN PathTags ON Paths.PathID = PathTags.PathID\n" +
                                 "WHERE Paths.PathID = 0\n" +
                                 "ORDER BY Paths.PathID";
-                        ResultSet paramTags_rs = stmt.executeQuery(paramTags_query);
+                        ResultSet paramTags_rs = stmt1.executeQuery(paramTags_query);
 
                         List<String> tag_list = new ArrayList<String>();
                         while(paramTags_rs.next()) {
@@ -122,15 +146,16 @@ public class Main {
                         pet_put.setTags(tag_list);
 
                         paramTags_rs.close();
+            System.out.println("Path Tags Ended");
 
 
                 //parameters
+            System.out.println("Parameter Started");
                 String parameter_query = "SELECT * FROM Paths \n" +
                         "LEFT JOIN Parameters ON Paths.PathID = Parameters.PathID\n" +
-                        "WHERE Paths.PathID = 2\n" +
+                        "WHERE Paths.PathID = 0\n" +
                         "ORDER BY Paths.PathID";
-                stmt2 = con.createStatement();
-                ResultSet parameters_rs = stmt2.executeQuery(parameter_query);
+                ResultSet parameters_rs = stmt1.executeQuery(parameter_query);
 
                 List<Parameter> parameter_list = new ArrayList<Parameter>();
 
@@ -166,9 +191,10 @@ public class Main {
                     Map<String,Schema> paramSchemaItems = new HashMap<String,Schema>();
                     String parameterItems_query = "SELECT * FROM Paths \n" +
                             "LEFT JOIN ParameterItems ON Paths.PathID = ParameterItems.PathID\n" +
-                            "WHERE Paths.PathID = 2 AND ParameterID = 0\n" +
+                            "WHERE Paths.PathID = 0 AND ParameterID = 0\n" +
                             "ORDER BY Paths.PathID;";
-                    ResultSet parameterItems_rs = stmt.executeQuery(parameterItems_query);
+                    ResultSet parameterItems_rs = stmt2.executeQuery(parameterItems_query);
+                    System.out.println("Parameter items Started");
 
                     Schema<String> parameterItems_schema = new Schema<String>();
                     List<String> parameterItemsEnum = new ArrayList<String>();
@@ -190,7 +216,10 @@ public class Main {
 
                     }
 
+
                     parameterItems_rs.close();
+
+                    System.out.println("Parameter Items Ended");
 
                     parameterItems_schema.setEnum(parameterItemsEnum);
 //                    System.out.println(parameterItems_schema.getEnum());
@@ -207,16 +236,19 @@ public class Main {
 
                 parameters_rs.close();
 
+            System.out.println("Parameters Ended");
+
 //            System.out.println(pet_put.getParameters());
 
                 //request body
+            System.out.println("Request Body Started");
                 String requestBody_query = "SELECT * FROM Paths\n" +
                         "LEFT JOIN RequestBody ON Paths.PathID = RequestBody.PathID\n" +
                         "WHERE Paths.PathID = 0\n" +
                         "ORDER BY Paths.PathID;";
-                ResultSet requestBody_rs = stmt.executeQuery(requestBody_query);
+                ResultSet requestBody_rs = stmt1.executeQuery(requestBody_query);
 
-            RequestBody requestBody_obj = new RequestBody();
+                RequestBody requestBody_obj = new RequestBody();
 
                 while(requestBody_rs.next()) {
                     String RBDescription = requestBody_rs.getString("Req_Description");
@@ -226,23 +258,25 @@ public class Main {
                     requestBody_obj.setRequired(RBIsRequired);
 
                     String requestBodyContent_query = "SELECT * FROM Paths\n" +
-                            "LEFT JOIN RequestBody ON Paths.PathID = RequestBody.PathID\n" +
+                            "LEFT JOIN RequestBodyContent ON Paths.PathID = RequestBodyContent.PathID\n" +
                             "WHERE Paths.PathID = 0\n" +
                             "ORDER BY Paths.PathID;";
                     ResultSet requestBodyContent_rs = stmt2.executeQuery(requestBodyContent_query);
 
+                    System.out.println("Request Body Content Started");
+
                     Content requestBodyContent_obj = new Content();
 
 
-                    requestBodyContent_rs.next();
-
-                    while(true){
-                        String RBContentName = requestBodyContent_rs.getString("ContentName");
-                        String RBContentProperty = requestBodyContent_rs.getString("SchemaProperties");
-                        String RBContentDescription = requestBodyContent_rs.getString("ContentDescription");
-                        String RBContentType = requestBodyContent_rs.getString("Type");
-                        String RBContentFormat = requestBodyContent_rs.getString("Format");
-                        String RBContentRef = requestBodyContent_rs.getString("Ref");
+                    if(requestBodyContent_rs.next()) {
+                        RBCLoop:
+                        while(true){
+                            String RBContentName = requestBodyContent_rs.getString("ContentName");
+                            String RBContentProperty = requestBodyContent_rs.getString("SchemaProperties");
+                            String RBContentDescription = requestBodyContent_rs.getString("ContentDescription");
+                            String RBContentType = requestBodyContent_rs.getString("Type");
+                            String RBContentFormat = requestBodyContent_rs.getString("Format");
+                            String RBContentRef = requestBodyContent_rs.getString("Ref");
 
 //                        if(RBContentName == null) {
                             //add to previous schema
@@ -282,7 +316,7 @@ public class Main {
                                         RBContentProperties_map.put(RBContentProperty2, requestBodyContent_propSchema2);
                                     }
                                 } else {
-                                    break;
+                                    break RBCLoop;
                                 }
                                 requestBodyContent_mainSchema.setProperties(RBContentProperties_map);
                                 requestBodyContent_media.setSchema(requestBodyContent_mainSchema);
@@ -291,42 +325,227 @@ public class Main {
                             }
 
 //                        }
-                    } //end of RBcontent while
+                            if(requestBodyContent_rs.next()){
+                                continue;
+                            } else {
+                                break;
+                            }
+                        }//end of RBcontent while
+                        requestBody_obj.setContent(requestBodyContent_obj);
+                    }
 
-                    requestBody_obj.setContent(requestBodyContent_obj);
+                    requestBodyContent_rs.close();
+
+                    System.out.println("Request Body Content Ended");
+
+
+
+
                 }
+
+            requestBody_rs.close();
+
+            System.out.println("Request Body Ended");
 
 
                 //responses
+                String response_query = "SELECT * FROM Paths\n" +
+                        "LEFT JOIN Responses ON Paths.PathID = Responses.PathID\n" +
+                        "WHERE Paths.PathID = 0\n" +
+                        "ORDER BY Paths.PathID;";
+                ResultSet Response_rs = stmt1.executeQuery(response_query);
+
+            System.out.println("Response Started");
+
+                ApiResponses Response_map = new ApiResponses();
+
+                while(Response_rs.next()) {
+
+                    ApiResponse Response_obj = new ApiResponse();
+
+                    int ResponseID = Response_rs.getInt("ResponseID");
+                    String HTTPStatus = Response_rs.getString("HTTPStatus");
+                    String responseDescription = Response_rs.getString("ResponseDescription");
+
+                    Response_obj.setDescription(responseDescription);
+
+                    String ResponseContent_query = "SELECT * FROM ResponseContent\n" +
+                            "WHERE ResponseID = 0 AND PathID = 0\n" +
+                            "ORDER BY ResponseContentID;";
+                    ResultSet ResponseContent_rs = stmt2.executeQuery(ResponseContent_query);
+
+                    System.out.println("Response Content Started");
+
+                    Content ResponseContent = new Content();
+
+                    while(ResponseContent_rs.next()) {
+                        MediaType ResponseContent_media = new MediaType();
+                        Schema ResponseContent_mainSchema = new Schema();
+
+
+                        int RCid = ResponseContent_rs.getInt("ResponseContentID");
+                        String RCName = ResponseContent_rs.getString("ContentItem");
+                        String RCType = ResponseContent_rs.getString("SchemaType");
+                        String RCRef = ResponseContent_rs.getString("SchemaRef");
+
+                        ResponseContent_mainSchema.setType(RCType);
+                        ResponseContent_mainSchema.set$ref(RCRef);
+
+
+                        String ResponseContentItem_query = "SELECT * FROM ResponseContentItem\n" +
+                                "WHERE ResponseContentID = 0 AND PathID = 0 AND ResponseID = 0\n" +
+                                "ORDER BY ResponseContentID;";
+                        ResultSet ResponseContentItem_rs = stmt3.executeQuery(ResponseContentItem_query); //stmt3
+
+                        System.out.println("Response Content Item Started");
+
+                        Map<String, Schema> ResponseContentProperties_map = new HashMap<String, Schema>();
+
+                        while(ResponseContentItem_rs.next()) {
+
+                            Schema ResponseContent_propSchema = new Schema();
+
+                            String RCItemName = ResponseContentItem_rs.getString("Item");
+                            String RCItemType = ResponseContentItem_rs.getString("Type");
+                            String RCItemRef = ResponseContentItem_rs.getString("$Ref");
+
+                            ResponseContent_propSchema.setType(RCItemType);
+                            ResponseContent_propSchema.set$ref(RCItemRef);
+
+                            ResponseContentProperties_map.put(RCItemName, ResponseContent_propSchema);
+                        }
+
+                        ResponseContentItem_rs.close();
+                        System.out.println("Response Content Item Ended");
+
+                        ResponseContent_mainSchema.setProperties(ResponseContentProperties_map);
+                        ResponseContent_media.setSchema(ResponseContent_mainSchema);
+                        ResponseContent.addMediaType(RCName, ResponseContent_media);
+                    }
+
+                    ResponseContent_rs.close();
+                    System.out.println("Response Content Ended");
+
+                    Response_obj.setContent(ResponseContent);
+                    Response_map.put(HTTPStatus, Response_obj);
+                }
+
+            Response_rs.close();
+            System.out.println("Response Ended");
+
+                pet_put.setResponses(Response_map);
+
+
+
+
                 //auth
+                String Security_query = "SELECT PathID, PathAuth.AuthID, PathAuth.ScopeID, AuthName, ScopeName FROM PathAuth\n" +
+                        "LEFT JOIN Auth ON PathAuth.AuthID = Auth.AuthID\n" +
+                        "LEFT JOIN Scope ON PathAuth.ScopeID = Scope.ScopeID\n" +
+                        "WHERE PathID = 0;";
+                ResultSet Security_rs = stmt1.executeQuery(Security_query);
+            System.out.println("SEcurity Started");
+
+                List<SecurityRequirement> SecurityList = new ArrayList<SecurityRequirement>();
+
+                SecurityRequirement Security_map = new SecurityRequirement();
+
+                Security_rs.next();
+
+                security:
+                while(true){
+                    List<String> ScopeNames = new ArrayList<String>();
+
+                    int SecurityAuthID = Security_rs.getInt("AuthID");
+                    String SecurityAuthName = Security_rs.getString("AuthName");
+                    String SecurityScopeName = Security_rs.getString("ScopeName");
+
+                    ScopeNames.add(SecurityScopeName);
+
+                    if(Security_rs.next()) {
+                        while (true) {
+                            if(Security_rs.getInt("AuthID") == SecurityAuthID) {
+                                ScopeNames.add(Security_rs.getString("ScopeName"));
+                                if(Security_rs.next()){
+                                    continue;
+                                }else {
+                                    Security_map.put(SecurityAuthName, ScopeNames);
+                                    break security;
+                                }
+
+                            } else {
+                                Security_map.put(SecurityAuthName, ScopeNames);
+                                break;
+                            }
+                        }
+
+                    } else {
+                        Security_map.put(SecurityAuthName, ScopeNames);
+                        break;
+                    }
+                }
+
+            Security_rs.close();
+            System.out.println("Security Ended");
+
+                SecurityList.add(Security_map);
+                pet_put.setSecurity(SecurityList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //            }
 
             paths_rs.close();//operation
 
+            System.out.println("Paths ended");
+
+            //components
 
 
 
-
-
+            System.out.println(io.swagger.util.Json.pretty(petstore));
 
         }catch(Exception e){
             e.printStackTrace();
         }finally{
+//            try{
+//                if(stmt!=null)
+//                    con.close();
+//            }catch(SQLException ignored){
+//            }
+//            try{
+//                if(con!=null)
+//                    con.close();
+//            }catch(SQLException se){
+//                se.printStackTrace();
+//            }
             try{
-                if(stmt!=null)
-                    con.close();
-            }catch(SQLException ignored){
-            }
-            try{
-                if(con!=null)
-                    con.close();
-            }catch(SQLException se){
-                se.printStackTrace();
+                con.close();
+            } catch (Exception e){
+                e.printStackTrace();
             }
         }
         System.out.println("Connecction closed!");
+
+
 
 
 
